019d005a8d734c76ddbc79b4a041cf765cc87682
David Torres <dwtorres@gmail.com>
2025-03-01 16:20:12 -0500

    Cleanup

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,4 +4,4 @@ module.exports = {
 	// We need this until https://github.com/dependabot/dependabot-core/issues/2445
 	// is resolved.
 	ignores: [(msg) => /Signed-off-by: dependabot\[bot]/m.test(msg)],
-  };
+};
